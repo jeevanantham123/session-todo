6008dff3b85ba2b4a945abcb208a21f19ceb1d33
jeevanantham123 <51525786+jeevanantham123@users.noreply.github.com>
2020-05-27 11:43:00 +0530

    Update database.sql

--- server/database.sql
+++ server/database.sql
@@ -1,19 +1,16 @@
-CREATE DATABASE perntodo;
+CREATE DATABASE todo;
 
-CREATE TABLE todo(
-  todo_id SERIAL PRIMARY KEY,
-  description VARCHAR(255)
+ create table user_details(
+ user_id serial PRIMARY KEY,
+ user_name varchar(255),
+ user_email varchar(255),
+ user_password varchar(255)
+ );
+
+
+create table todo_list(
+todo_id serial PRIMARY KEY,
+description varchar(255),
+user_id integer references user_details(user_id)
 );
 
-
-
-CREATE TABLE todo_list(
- todo_id SERIAL PRIMARY KEY,
-description VARCHAR(255),
- user_name VARCHAR(255)
- );
-
-
- CREATE TABLE todo_user(
- curr_user VARCHAR(255)
- );
